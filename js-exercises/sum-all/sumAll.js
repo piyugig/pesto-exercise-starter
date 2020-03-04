468b0969b9b4376c65531b2c3be671fe6638ad72
Piyush Gupta <piyush.gig@live.com>
2020-03-04 16:24:55 +0530

    Solution sumAll

--- js-exercises/sum-all/sumAll.js
+++ js-exercises/sum-all/sumAll.js
@@ -1,7 +1,14 @@
-function sumAll() {
-  
+function sort(inputIntegerArray) {
+  return inputIntegerArray.sort((a, b) => a - b);
 }
 
-export {
-  sumAll,
-};
+function sumAll(rangeArray) {
+  sort(rangeArray);
+  let sum = 0;
+  for (let i = rangeArray[0]; i <= rangeArray[1]; i += 1) {
+    sum += i;
+  }
+  return sum;
+}
+
+export { sumAll };
