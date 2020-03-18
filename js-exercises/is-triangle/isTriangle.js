f7d89a2938aaf8fc43f4cbdda22f6ffe6f6d89da
Piyush G <piyush.gig@live.com>
2020-03-18 16:02:38 +0530

    solution isTriangle

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,5 +1,15 @@
-function isTriangle() {
-
+function isTriangle(...args) {
+  try {
+    for (const element of args) {
+      if (typeof element !== 'number') {
+        throw new Error(`Expected number for agrument, got ${element} of type ${typeof element}`);
+      }
+    }
+    const [line1, line2, line3] = [...args];
+    return line1 + line2 > line3 && line2 + line3 > line1 && line3 + line1 > line2;
+  } catch (err) {
+    return err;
+  }
 }
 
 export {
